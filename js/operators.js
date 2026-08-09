0f675607c9b893c814f94675ce23c008b8a735d4
Ayauelemes <dana26032007@icloud.com>
2026-08-09 15:19:43 +0500

    operators

---
+++ js/operators.js
@@ -0,0 +1,55 @@
+//1
+let a = 10;
+let b = 5;
+let c = 2;
+console.log((a+b+c));
+console.log((a+c)-b);
+console.log((a*b)/c);
+console.log(a%b);
+
+//2
+let x=7;
+++x;
+console.log(x);
+
+let x1 =7;
+x1++;
+console.log(x1);
+
+let x2=7;
+--x2;
+console.log(x2);
+
+let x3=7;
+x3--;
+console.log(x3);
+
+//3
+let num1 = 4;
+let num2=5;
+let result=num1+num2;
+console.log("The result is "+result);
+
+//4
+let a1=7;
+let b1=4;
+let ulken = a1>b1;
+console.log(ulken);
+
+let c1=2;
+console.log(c1<=2);
+
+console.log((a1/c1)!=5);
+
+//5
+let san = 10;
+let san1 = "10";
+console.log(san ==san1); //тип данныхқа қарамай салыстырады
+console.log(san ===san1); //мында екеуінде салыстырады типті де мәнінде
+
+//6
+let xTrue = 10;
+console.log(xTrue/2==0 ||xTrue/3==0|| xTrue/6!=0);
+
+let yFalse = 0;
+console.log(yFalse<10 &&yFalse>5);
